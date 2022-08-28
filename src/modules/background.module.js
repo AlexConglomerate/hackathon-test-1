04495308c428527a53d95705637fade367feecb6
xibidzh <4illoff@gmail.com>
2022-08-28 17:30:08 +0300

    123

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,22 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
+import { getRandomColor } from "../utils";
+import { random } from "core-js/core/number";
 
 export class BackgroundModule extends Module {
+  constructor() {
+    super("background", "Случайный фоновый цвет");
+  }
 
+  trigger() {
+    const colorOne = getRandomColor();
+    const colorTwo = getRandomColor();
+
+    document.body.style.background = `
+			linear-gradient(${random(0, 360)}deg, ${colorOne}, ${colorTwo})
+		`;
+
+    setTimeout(() => {
+      document.body.style.background = "";
+    }, 3000);
+  }
 }

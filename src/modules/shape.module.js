714a3ec6a63d3cf1a7e955274cdcd5a1f8d516a9
xibidzh <4illoff@gmail.com>
2022-08-28 19:14:22 +0300

    123

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -13,11 +13,18 @@ export class ShapeModule extends Module {
 
     this.shapeEl.style.height = shapeHeight + "px";
     this.shapeEl.style.width = shapeWidth + "px";
-    this.shapeEl.style.background = getRandomColor();
     this.shapeEl.style.borderRadius = random(4, 80) + "%";
+    this.shapeEl.style.background = getRandomColor();
     this.shapeEl.style.position = "absolute";
-    this.shapeEl.style.top = random(20, 900) + "px";
-    this.shapeEl.style.left = random(20, 900) + "px";
+    this.shapeEl.style.top =
+      random(0, document.documentElement.clientHeight - shapeHeight) + "px";
+    this.shapeEl.style.left =
+      random(0, document.documentElement.clientHeight - shapeWidth) + "px";
     document.body.append(this.shapeEl);
+
+    const timeToKill = setTimeout(() => {
+      this.shapeEl.remove();
+      clearTimeout(timeToKill);
+    }, 6000);
   }
 }
